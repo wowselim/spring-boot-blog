57ed2ad2a92b942c45f387f4069dd518ab92fdb7
Selim Dincer <wowselim@live.de>
2018-07-19 11:45:08 +0200

    switch to npm repo and add mithril

--- src/main/java/co/selim/blog/controller/PageController.java
+++ src/main/java/co/selim/blog/controller/PageController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.Map;
 
 /**
+ * Serves the initial SPA page.
  * Created by Selim on 10.03.2018.
  */
 @Controller
-public class PageController extends AbstractController {
+public class PageController {
     @GetMapping("/index")
     public String index(Map<String, Object> model) {
+        model.put("title", "selim.co");
         return "index";
     }
 }
